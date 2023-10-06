0ce5f4d30a2b188c7186eee07a59518f4e59e367
micaelachayo <micaelachayo@gmail.com>
2023-10-05 22:52:52 -0300

    primer commit, desarrollo hecho. empezando js

---
+++ README.md
@@ -0,0 +1,13 @@
+# PreEntrega3_Benayon
+
+Dasa es un sitio web de ropa infantil,desde 0 a 12 años.
+
+Este proyecto está pensado para la preentrega 3 del curso de desarrollo web,utilizando bootstrap.
+Agregandole SASS, animaciones, transformaciones y seo (imagenes webp y svg para íconos, Meta descripciones y meta keyword).
+Todas las paginas están responsive desde telefono 360px.
+
+La libreria que utilizé para las entradas de titulos y cards.
+michalsnik.github.io/aos
+
+sitio web creado por Micaela Benayon
+
---
+++ js/main.js
@@ -0,0 +1,87 @@
+
+
+
+//funciones
+function prendasAbuscar() {
+  prenda = prompt(`¿Qué prenda busca para su ${categoria}? (Ingrese "nada" para terminar)`);
+}
+
+function infoPrenda(precio) {
+  montoTotal += precio;
+  alert(`la prenda que busca ${prenda} sale ${precio} .\nMonto total hasta ahora $` + montoTotal);
+  console.log(`${montoTotal}`);
+}
+ function noTenemosPrenda(){
+    alert("Lamentamos, no tenemos esa prenda en la tienda");
+ }
+
+
+
+ while (true) {
+   let entrada = prompt("Ingrese su nombre por favor");
+  
+    if (isNaN(entrada)) {
+      alert(`Bienvenido a Dasa ${entrada}`);
+      break; // Sale del bucle si la entrada es un nombre válido
+    } else {
+      alert("Por favor, ingrese un nombre en lugar de un número.");
+    }
+  }
+
+ let categoria = prompt("¿Qué categoría de prendas estás buscando? (bebe (0 a 3)/nena (4 a 14)/nene (4-14))");
+ let prenda;
+ let montoTotal = 0;
+//Bucle
+switch (categoria) {
+  case "bebe":
+    prendasAbuscar();
+    while (prenda.toLowerCase() !== "nada") { 
+      switch (prenda) {
+        case "body":
+          infoPrenda(6400);
+          break;
+        case "enterito":
+          infoPrenda(4780);
+          break;
+        default:
+          noTenemosPrenda();
+      }
+      prendasAbuscar(); // Pedir otra prenda
+    }
+    break;
+
+    case "nena":
+      prendasAbuscar();
+      while (prenda.toLowerCase() !== "nada") {
+        switch (prenda) {
+          case "vestido":
+            infoPrenda(8100);
+            break;
+          case "pollera":
+            infoPrenda(5600);
+            break;
+          default:
+            noTenemosPrenda();
+        }
+        prendasAbuscar(); // Pedir otra prenda
+      }
+      break;
+      case "nene":
+    prendasAbuscar();
+    while (prenda.toLowerCase() !== "nada") { 
+      switch (prenda) {
+        case "camisa":
+          infoPrenda(7300);
+          break;
+        case "pantalon":
+          infoPrenda(10780);
+          break;
+        default:
+          noTenemosPrenda();
+      }
+      prendasAbuscar(); // Pedir otra prenda
+    }
+    break;
+  default:
+    alert("Lamentamos, no tenemos esa categoría de prendas en la tienda");
+}
