05416e9fc317a285f7c52c4d9424c07cd20272ff
micaelachayo <micaelachayo@gmail.com>
2023-10-26 11:25:50 -0300

    agregando objetos, arrays y metodos

--- js/main.js
+++ js/main.js
@@ -1,87 +1,85 @@
 
 
+//VARIABES
+let montoTotal = 0;
+let carrito = [];
+let categoria;
 
-//funciones
-function prendasAbuscar() {
-  prenda = prompt(`¿Qué prenda busca para su ${categoria}? (Ingrese "nada" para terminar)`);
-}
+// Arrays para almacenar información sobre las prendas y categorías
+let opciones = ["bebes", "nenas", "nenes"];
+let prendas = {
+  bebes: ["body", "enterito"],
+  nenes: ["camisa", "pantalon"],
+  nenas: ["remera", "pollera", "vestido"],
+};
 
-function infoPrenda(precio) {
+let precios = {
+  body: 6400,
+  enterito: 4780,
+  vestido: 8100,
+  pollera: 5600,
+  camisa: 7300,
+  pantalon: 10780,
+  remera: 32044,
+};
+
+//FUNCIONES
+const buscarPrenda = () => {
+  prenda = prompt(
+    `¿Qué prenda busca para su ${categoria}? (Ingrese "nada" para terminar)`
+  );
+};
+
+const preciosPrenda = (prenda, precio) => {
   montoTotal += precio;
-  alert(`la prenda que busca ${prenda} sale ${precio} .\nMonto total hasta ahora $` + montoTotal);
-  console.log(`${montoTotal}`);
-}
- function noTenemosPrenda(){
-    alert("Lamentamos, no tenemos esa prenda en la tienda");
- }
+  alert(
+    `La prenda que busca ${prenda} cuesta $${precio}.\nMonto total hasta ahora $${montoTotal}`
+  );
+  console.log(`Monto total: $${montoTotal}`);
+};
 
+const respuestaCarrito = (prenda, precio) => {
+  montoTotal += precio;
+  const respuesta = prompt("¿Desea agregar la prenda al carrito? (si/no)");
+  if (respuesta.toLowerCase() === "si") {
+    carrito.forEach((ropa) => console.log(ropa));
+    carrito.push(`${prenda} = $${precio}`);
+    alert(`Su prenda ${prenda} fue añadida al carrito`);
+  }
+};
 
+const noTenemosPrenda = () => {
+  alert("Lamentamos, esa prenda no la tenemos en nuestra tienda");
+};
 
- while (true) {
-   let entrada = prompt("Ingrese su nombre por favor");
-  
-    if (isNaN(entrada)) {
-      alert(`Bienvenido a Dasa ${entrada}`);
-      break; // Sale del bucle si la entrada es un nombre válido
+// FLUJO PRINCIPAL DEL PROGRAMA
+categoria = prompt("Hola, Bienvenid@ a Dasa! \n¿Qué categoría de prendas estás buscando? (bebes/nenas/nenes)?  ");
+
+if (opciones.includes(categoria)) {
+  buscarPrenda();
+  while (prenda.toLowerCase() !== "nada") {
+    if (prendas[categoria].includes(prenda)) {
+      preciosPrenda(prenda, precios[prenda]);
+      respuestaCarrito(prenda, precios[prenda]);
     } else {
-      alert("Por favor, ingrese un nombre en lugar de un número.");
+      noTenemosPrenda();
+    }
+    const continuarBuscando = prompt(
+      "¿Desea seguir buscando otra prenda? (Sí/No)"
+    ).toLowerCase();
+    if (continuarBuscando === "si") {
+      buscarPrenda();
+    } else {
+      break; // Sale del bucle si no desea buscar más prendas
     }
   }
 
- let categoria = prompt("¿Qué categoría de prendas estás buscando? (bebe (0 a 3)/nena (4 a 14)/nene (4-14))");
- let prenda;
- let montoTotal = 0;
-//Bucle
-switch (categoria) {
-  case "bebe":
-    prendasAbuscar();
-    while (prenda.toLowerCase() !== "nada") { 
-      switch (prenda) {
-        case "body":
-          infoPrenda(6400);
-          break;
-        case "enterito":
-          infoPrenda(4780);
-          break;
-        default:
-          noTenemosPrenda();
-      }
-      prendasAbuscar(); // Pedir otra prenda
-    }
-    break;
-
-    case "nena":
-      prendasAbuscar();
-      while (prenda.toLowerCase() !== "nada") {
-        switch (prenda) {
-          case "vestido":
-            infoPrenda(8100);
-            break;
-          case "pollera":
-            infoPrenda(5600);
-            break;
-          default:
-            noTenemosPrenda();
-        }
-        prendasAbuscar(); // Pedir otra prenda
-      }
-      break;
-      case "nene":
-    prendasAbuscar();
-    while (prenda.toLowerCase() !== "nada") { 
-      switch (prenda) {
-        case "camisa":
-          infoPrenda(7300);
-          break;
-        case "pantalon":
-          infoPrenda(10780);
-          break;
-        default:
-          noTenemosPrenda();
-      }
-      prendasAbuscar(); // Pedir otra prenda
-    }
-    break;
-  default:
-    alert("Lamentamos, no tenemos esa categoría de prendas en la tienda");
+  // Mostrar el contenido del carrito
+  if (carrito.length > 0) {
+    alert(`Contenido del carrito: ${carrito.join(", ")}. \nGracias por elegirnos!!`);
+  } else {
+    alert("Gracias, nos vemos la próxima");
+  }
+} else {
+  alert("Lamentamos, no tenemos esa categoría de prendas en la tienda");
 }
